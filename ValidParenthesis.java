220c08c60dd964ee3015b3f3b58b479f270fcd36
ashwani-developer <ashwanigarg.developer@gmail.com>
2025-03-31 17:46:34 +0530

    valid apranthesis using stream

--- ValidParenthesis.java
+++ ValidParenthesis.java
@@ -1,4 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class ValidParenthesis {
@@ -9,17 +12,39 @@ public class ValidParenthesis {
 
         String st = "()()()()()()()()()()()()()()()";
         Stack<Character> stck = new Stack<>();
-
         for (int t = 0; t < st.length(); t++) {
-            if (st.charAt(t) == '{' || st.charAt(t) == '[' || st.charAt(t) == '(') {
-                stck.push(st.charAt(t));
-            } else if ((st.charAt(t) == '}' && stck.peek() == '{') || (st.charAt(t) == ')' && stck.peek() == '(') || (st.charAt(t) == ']' && stck.peek() == '[')) {
+            char c = st.charAt(t);
+            if (c == '{' || c == '[' || c == '(') {
+                stck.push(c);
+            } else if (!stck.isEmpty() &&
+                    ((c == '}' && stck.peek() == '{') ||
+                            (c == ')' && stck.peek() == '(') ||
+                            (c == ']' && stck.peek() == '['))) {
                 stck.pop();
+            } else {
+                // If a closing bracket does not match, it's invalid
+                System.out.println("Invalid String");
+                return;
             }
-
         }
         System.out.println("Valid stingg" + !(stck.size() > 0));
+        Deque<Character> stack = new ArrayDeque<>();
 
-
+        boolean isValid = IntStream.range(0, st.length())
+                .mapToObj(st::charAt)
+                .allMatch(c -> {
+                    if (c == '{' || c == '[' || c == '(') {
+                        stack.push(c);
+                        return true;
+                    } else if (!stack.isEmpty() &&
+                            ((c == '}' && stack.peek() == '{') ||
+                                    (c == ')' && stack.peek() == '(') ||
+                                    (c == ']' && stack.peek() == '['))) {
+                        stack.pop();
+                        return true;
+                    }
+                    return false; // Invalid case
+                });
+        System.out.println("is this valid"+ (isValid && stack.isEmpty()));
     }
 }
